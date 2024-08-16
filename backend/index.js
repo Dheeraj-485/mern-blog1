5812ff8bc61691629dca26a7299c488c52a0caa4
Dheeraj-485 <118504120+Dheeraj-485@users.noreply.github.com>
2024-08-16 14:15:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,38 @@
-const express = require("express")
-const path = require("path")
-const userRoutes = require("./routes/userRouter")
-const blogRoutes = require("./routes/blogRouter")
-const commentRoutes = require("./routes/commentRouter")
-const cors = require("cors")
-const connectToMongoDB = require("./db")
+const express = require("express");
+const path = require("path");
+const userRoutes = require("./routes/userRouter");
+const blogRoutes = require("./routes/blogRouter");
+const commentRoutes = require("./routes/commentRouter");
+const cors = require("cors");
+const connectToMongoDB = require("./db");
+// const fileUpload = require("express-fileupload");
+const fileUpload = require("express-fileupload");
 
-const app = express()
+const cloudinary = require("cloudinary");
 
-const PORT = process.env.PORT || 8000
-connectToMongoDB()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static(path.resolve("./public")))
-app.use("/user", userRoutes)
-app.use("/blog", blogRoutes)
-app.use("/comment", commentRoutes)
+const PORT = process.env.PORT || 8000;
+connectToMongoDB();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.resolve("./public")));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+app.use("/user", userRoutes);
+app.use("/blog", blogRoutes);
+app.use("/comment", commentRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening at the PORT : ${PORT}`)
-})
+  console.log(`Server is listening at the PORT : ${PORT}`);
+});
