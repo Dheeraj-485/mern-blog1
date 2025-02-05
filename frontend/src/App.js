ca3ef7377181f53177b00dd619056f90c9b44a93
Dheeraj-485 <118504120+Dheeraj-485@users.noreply.github.com>
2025-02-05 14:54:56 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,76 @@
-import { Toaster } from "react-hot-toast";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+// import { Toaster } from "react-hot-toast";
 
-import {
-  createBrowserRouter,
-  Route,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
+// import {
+//   createBrowserRouter,
+//   Route,
+//   createRoutesFromElements,
+//   RouterProvider,
+//   Outlet,
+// } from "react-router-dom";
+// import {
+//   Home,
+//   Login,
+//   Signup,
+//   CreateBlog,
+//   NotFound,
+//   Blog,
+//   Blogs,
+//   BlogDetails,
+//   Navbar,
+//   MyBlogs,
+//   DashBoard,
+//   EditBlog,
+// } from "./pages/index";
+// import SearchResults from "./pages/SearchQuery";
+// import Footer from "./pages/Footer";
+// import Contact from "./pages/Contact";
+
+// const Layout = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <Outlet />
+//       <Footer />
+//     </>
+//   );
+// };
+// //create router
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <>
+//       <Route path="/" element={<Navbar />}>
+//         <Route path="/search" element={<SearchResults />} />
+
+//         <Route index element={<Home />} />
+//         <Route path="signup" element={<Signup />} />
+//         <Route path="login" element={<Login />} />
+//         <Route path="blogs" element={<Blogs />} />
+//         <Route path="blogs/:id" element={<BlogDetails />} />
+//         <Route path="createblog" element={<CreateBlog />} />
+//         <Route path="blog" element={<Blog />} />
+//         <Route path="myblogs" element={<MyBlogs />} />
+//         <Route path="dashboard" element={<DashBoard />} />
+//         <Route path="editblog/:id" element={<EditBlog />} />
+//         <Route path="contact" element={<Contact />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Route>
+//       {/* <Footer /> */}
+//     </>
+//   )
+// );
+// function App() {
+//   return (
+//     <div>
+//       <RouterProvider router={router} />
+//       <Toaster />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import {
   Home,
   Login,
@@ -22,33 +85,45 @@ import {
   DashBoard,
   EditBlog,
 } from "./pages/index";
-import { Footer } from "./pages/Footer";
+import SearchResults from "./pages/SearchQuery";
+import Footer from "./pages/Footer";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
-//create router
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Navbar />}>
-      <Route index element={<Home />} />
-      <Route path="signup" element={<Signup />} />
-      <Route path="login" element={<Login />} />
-      <Route path="blogs" element={<Blogs />} />
-      <Route path="blogs/:id" element={<BlogDetails />} />
-      <Route path="createblog" element={<CreateBlog />} />
-      <Route path="blog" element={<Blog />} />
-      <Route path="myblogs" element={<MyBlogs />} />
-      <Route path="dashboard" element={<DashBoard />} />
-      <Route path="editblog/:id" element={<EditBlog />} />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
+// Layout Component to wrap Navbar, Page Content, and Footer
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <div className="main-content">{children}</div> {/* Allows flex-grow */}
+      <Footer />
+    </>
+  );
+};
+
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchResults />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/:id" element={<BlogDetails />} />
+          <Route path="/createblog" element={<CreateBlog />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/myblogs" element={<MyBlogs />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/editblog/:id" element={<EditBlog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Layout>
       <Toaster />
-      {/* <Footer /> */}
-    </div>
+    </Router>
   );
 }
 
